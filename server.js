7aa21233fe49a4377b666ebc5ea52d6f63de91ba
Trent Davis <trentdavisinc@gmail.com>
2019-03-24 21:28:18 -0500

    Fix Heroku

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const PORT = process.env.PORT || 3000;
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var apiRoutes = require('./app/routing/apiRoutes');
-var htmlRoutes = require('./app/routing/htmlRoutes');
 
 var app = express();
 
-app.use('/api/friends', apiRoutes);
-app.use('*', htmlRoutes);
 
-app.listen(PORT, () => console.log('App listening on port: ' + PORT));
+var PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
